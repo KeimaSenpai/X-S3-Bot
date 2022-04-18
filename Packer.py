28878dba9f150b935a19633a269eab201dbfeedc
Keima Senpai <98184310+KeimaSenpai@users.noreply.github.com>
2022-04-17 22:40:56 -0400

    Add files via upload

---
+++ Packer.py
@@ -0,0 +1,12 @@
+import os
+import zipfile
+
+def packFolder(fold,zip):
+    files = os.listdir(fold)
+    for f in files:
+        filepath = fold+'\\'+f
+        if os.path.isfile(filepath):
+            zip.write(filepath,f)
+            os.unlink(filepath)
+        else:
+            packFolder(f,zip)
